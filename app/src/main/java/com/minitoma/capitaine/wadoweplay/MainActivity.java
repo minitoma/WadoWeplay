878edb1c3bfc9c857371dbdf12e2604b26a3c627
minitoma <calatayud.thomas@hotmail.fr>
2018-04-05 22:25:13 +0200

    Commencement ViewPager

--- app/src/main/java/com/minitoma/capitaine/wadoweplay/MainActivity.java
+++ app/src/main/java/com/minitoma/capitaine/wadoweplay/MainActivity.java
@@ -1,13 +1,37 @@
 package com.minitoma.capitaine.wadoweplay;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+import java.util.Vector;
+
+public class MainActivity extends FragmentActivity {
+
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Création de la liste de Fragment que fera défiler le PagerAdapter
+        List fragments = new Vector();
+
+        // Ajout des Fragments dans la liste
+        fragments.add(Fragment.instantiate(this, ListFriendFragment.class.getName()));
+        fragments.add(Fragment.instantiate(this,StatusFeedFragment.class.getName()));
+
+        // Création de l'adapter qui s'occupera de l'affichage de la liste de Fragments
+        this.mPagerAdapter = new MyPagerAdapter(super.getSupportFragmentManager(), fragments );
+
+        ViewPager pager = (ViewPager) super.findViewById(R.id.viewpager);
+        // Affectation de l'adapter au ViewPager
+        pager.setAdapter(this.mPagerAdapter);
+
     }
 }
